860e7c2204ca14cd1177e9f620e62f163cdffb85
nickorfitelli <nicholas.orfitelli.1@us.af.mil>
2020-07-20 15:21:13 -0400

    volume

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,6 +16,25 @@ public class HelloController {
     @GetMapping("/math/pi")
     public double getIndex() {
         return 3.141592653589793;
+    }
+
+//    @GetMapping("/name")
+//    public String getName(
+//
+//            @RequestParam(value = "firstname", required=false, defaultValue = "jean-marc")
+//                    String firstName,
+//            @RequestParam(value = "lastname", required=false, defaultValue = "julien") String lastName)
+//
+//    {
+//        return "Hello "+firstName+" "+lastName;
+//    }
+
+
+    @RequestMapping("/math/volume/{length}/{width}/{height}")
+    public String getIndividualParams(@PathVariable int length, @PathVariable int width, @PathVariable int height) {
+        int vol = length*width*height;
+        String ret = Integer.toString(vol);
+        return "The volume is: " + ret;
     }
 
 }
